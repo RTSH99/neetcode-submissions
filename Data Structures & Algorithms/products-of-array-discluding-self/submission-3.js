8440eab73cb2df7986613caab631d0ac30c2447f
Ritwik Sharma <101930943+RTSH99@users.noreply.github.com>
2026-08-04 23:20:56 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+       const res = new Array(nums.length).fill(1)
+       for(let i=1;i<nums.length;i++){
+            res[i]=nums[i-1]*res[i-1]
+       }
+       let postFix=1
+       for(let i= nums.length-1;i>=0;i--){
+        res[i]*=postFix
+        postFix*=nums[i]
+       }
+       return res;
+    }
+}
