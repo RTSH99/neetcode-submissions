d342029bc2ef04a6cfb2fe683791d2865bc67c1f
Ritwik Sharma <101930943+RTSH99@users.noreply.github.com>
2026-08-18 12:32:55 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+         let l =1
+    let r =Math.max(...piles)
+    let res = r
+    while(l<=r){
+        let hours =0
+        let k = Math.round((l+r)/2)
+        for (let p of piles){
+            hours += Math.ceil(p/k)
+        }
+        if(hours<=h){
+            res = Math.min(res,k)
+            r=k-1
+        }
+        else{
+            l=k+1
+        }
+    }
+    return res
+    }
+}
