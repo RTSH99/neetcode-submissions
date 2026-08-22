fdba7db8a672a852b228c719d47339bdeca3f6ee
Ritwik Sharma <101930943+RTSH99@users.noreply.github.com>
2026-08-23 01:01:47 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+           let l=0 //buy
+      let r=1 //sell
+      let maxProfit =0
+      while(r<prices.length){
+          if(prices[l]<prices[r]){
+              let profit = prices[r] - prices[l]
+              maxProfit = Math.max(maxProfit,profit)
+          }
+          else {
+              l=r
+          }
+          r++
+      }
+      return maxProfit
+    }
+}
